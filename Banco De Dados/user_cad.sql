cba3fdd77758e469f4f3dda6f06363484d0d2f48
João Victor Ferreira de Aragão <jojoba2018cnx@gmail.com>
2023-03-02 12:05:38 -0300

    Criação das tabelas de categorias e itens

---
+++ Banco De Dados/user_cad.sql
@@ -0,0 +1,11 @@
+create database users;
+use users;
+
+create table user_cad(
+	id INT auto_increment primary KEY,
+	username varchar(255) not null,
+	login varchar(255) not null,
+    senha varchar(255) not null
+);
+
+select senha from user_cad where login ='admin@gmail.com';
