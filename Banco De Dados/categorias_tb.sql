173ec176724c21bba0e860b5360318eefca59cec
João Victor Ferreira de Aragão <jojoba2018cnx@gmail.com>
2026-01-20 17:34:19 -0300

    Começando do 0

--- Banco De Dados/categorias_tb.sql
+++
@@ -1,9 +0,0 @@
-use users;
-
-create table categorias_tb(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    NOME VARCHAR(255),
-    DESCRICAO VARCHAR(255)
-);
-
-select * from categorias_tb;
--- test.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo password_hash('$2y$10$w3lhiA3S1Bxzu0M1ACJeT.F0kN6iVUQpaRVwLLZWnK/qIqgdAj0Pe', PASSWORD_DEFAULT);
